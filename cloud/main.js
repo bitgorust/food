07f1d062da76550e9928c06bc7d3203b5ca842d2
曾劲 <king.fudan@gmail.com>
2014-05-24 20:33:12 +0800

    add diet api

--- cloud/main.js
+++ cloud/main.js
@@ -3,3 +3,21 @@
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+AV.Cloud.define("diet", function(request, response) {
+    var Order = AV.Object.extend("Order");
+    var Food = AV.Object.extend("Food");
+    var orderQuery = new AV.Query(Order);
+    orderQuery.equalTo("u_id", request.params.u_id);
+    var foodQuery = new AV.Query(Food);
+    foodQuery.matchesKeyInQuery("f_name", "name", orderQuery);
+    foodQuery.include(["r_name"]);
+    foodQuery.find({
+        success: function(foods) {
+            response.success(foods);
+        }
+        error: function() {
+            response.error("user diet lookup failed");
+        }
+    });
+}); 
