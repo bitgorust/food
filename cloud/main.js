8f578b77ae418506be87d2c3a1ef2fdeb92535fb
曾劲 <king.fudan@gmail.com>
2014-05-24 22:53:58 +0800

    diet return food analysis

--- cloud/main.js
+++ cloud/main.js
@@ -10,14 +10,40 @@ AV.Cloud.define("diet", function(request, response) {
     var orderQuery = new AV.Query(Order);
     orderQuery.equalTo("u_id", request.params.u_id);
     var foodQuery = new AV.Query(Food);
-    foodQuery.matchesKeyInQuery("f_name", "name", orderQuery);
-    foodQuery.include(["r_name"]);
-    foodQuery.find({
-        success: function(foods) {
-            response.success(foods);
+    foodQuery.matchesKeyInQuery("name", "f_name", orderQuery);
+    foodQuery.select("name", "r_id", "shucai", "weidao", "roulei", "zhushi", "caixi");
+    foodQuery.find().then(function(foods) {
+        var shucai = {}, weidao = {}, roulei = {}, zhushi = {}, caixi = {};
+        for (var i = 0; i < foods.length; ++i) {
+            add_to_map(shucai, foods[i].get("shucai"));
+            add_to_map(weidao, foods[i].get("weidao"));
+            add_to_map(roulei, foods[i].get("roulei"));
+            add_to_map(zhushi, foods[i].get("zhushi"));
+            add_to_map(caixi,  foods[i].get("caixi"));
         }
-        error: function() {
-            response.error("user diet lookup failed");
-        }
+        return {
+            "shucai" : shucai,
+            "weidao" : weidao,
+            "roulei" : roulei,
+            "zhushi" : zhushi,
+            "caixi"  : caixi
+            };
+    }).then(function(diet) {
+        response.success(diet);
+    }, function(error) {
+        response.error("user diet get failed");
     });
 }); 
+
+function add_to_map(map, item) {
+    if (item && item !== "") {
+        var strs = item.split(",");
+        for (var i = 0; i < strs.length; ++i) {
+            if (strs[i] in map) {
+                map[strs[i]] = map[strs[i]] + 1;
+            } else {
+                map[strs[i]] = 1;
+            }
+        }
+    }
+}
